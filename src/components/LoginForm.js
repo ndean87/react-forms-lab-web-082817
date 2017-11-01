ccc7019624806853df93c9b5eded2faee0c6320c
ndean87 <ndean87@gmail.com>
2017-11-01 15:44:37 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
+  onUserInputChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  onPasswordInputChange = (event) => {
+    this.setState({
+      password: event.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.onUserInputChange} />
           </label>
+
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.onPasswordInputChange} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,47 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  checkError = () => {
+    const poem = this.state.value
+    const lines = poem.split("\n")
+    if(lines.length === 3){
+      let lineOne = lines[0].trim().split(/\s+/).length;
+      let lineTwo = lines[1].trim().split(/\s+/).length
+      let lineThree = lines[2].trim().split(/\s+/).length
+      if(lineOne === 5 && lineTwo === 3 && lineThree === 5 ){
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
 
   render() {
+    const error = this.checkError() ? null : (<div
+      id="poem-validation-error"
+      style={{color: 'red'}}
+    >
+      This poem is not written in the right structure!
+    </div>)
     return (
       <div>
         <textarea
           rows="3"
           cols="60"
-        />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+          onChange={this.onInputChange}
+        >{this.state.value}</textarea>
+        {error}
       </div>
     );
   }
